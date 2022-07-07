149514033dc02f56c160e245d310cb5bba0874bf
AXON <axonasif@gmail.com>
2022-07-07 11:54:30 +0000

    refactor for openvscode-server

--- patch.sh
+++ patch.sh
@@ -1,18 +1,18 @@
 #!/bin/bash
-if [ -z $1 ]; then
-  CODE_SERVER_PATH="/usr/lib/code-server"
-else
-  CODE_SERVER_PATH="$1"
-fi
+# if [ -z $1 ]; then
+#   CODE_SERVER_PATH="/usr/lib/code-server"
+# else
+#   CODE_SERVER_PATH="$1"
+# fi
 
-WORKBENCH_PATH="$CODE_SERVER_PATH/lib/vscode/out/vs/workbench"
+WORKBENCH_PATH="/ide/out/vs/workbench"
 WORKBENCH_CSS_PATH="$WORKBENCH_PATH/workbench.web.main.css"
 
-# Check if it is valid path
-if [ -z "$CODE_SERVER_PATH" -o ! -d "$WORKBENCH_PATH" ]; then
-  echo "Please give valid code-server path!"
-  exit 1
-fi
+# # Check if it is valid path
+# if [ -z "$CODE_SERVER_PATH" -o ! -d "$WORKBENCH_PATH" ]; then
+#   echo "Please give valid code-server path!"
+#   exit 1
+# fi
 
 if ! grep -q "/* ::CUSTOM VSCODE FONTS:: */" "$WORKBENCH_CSS_PATH"; then
   # Copy fonts to $WORKBENCH_PATH
